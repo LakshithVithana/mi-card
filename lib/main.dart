55114c88fc00c621d3a77f235bd8ff5209d7fcd8
LakshithVithana <lakshithlfvithana@gmail.com>
2020-06-14 18:33:32 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,72 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
                 CircleAvatar(
                 radius: 50.0,
                 backgroundImage: AssetImage('images/me.jpg'),
               ),
-              Text('Lakshith V'),
+              Text(
+                'Lakshith Vithana',
+                style: TextStyle(
+                  fontFamily: 'Lobster',
+                  fontSize: 26.0,
+                  color: Colors.green[50],
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Teko',
+                  fontSize: 18.0,
+                  color: Colors.white,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color:Colors.teal[100],
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal[900],
+                  ),
+                  title: Text(
+                    '+94 70 235 1407',
+                    style: TextStyle(
+                      color: Colors.teal[900],
+                      fontFamily: 'Teko',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal[900],
+                  ),
+                  title: Text(
+                    'lakshith@gmail.com',
+                    style: TextStyle(
+                      color: Colors.teal[900],
+                      fontFamily: 'Teko',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
             ]
           ),
         ),
